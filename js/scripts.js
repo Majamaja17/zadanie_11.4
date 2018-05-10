4ec9f18996b183fd5f3394a51732869224bd45fd
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-10 13:07:15 +0200

    Change js and html files

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
-window.onload = function() {
-
-	var withButtonItems = document.getElementsByClassName("button");
-	console.log(withButtonItems);
-
-	var buttonItemsLength = withButtonItems.length;
-	console.log(buttonItemsLength);
-
-	for (var i = 0; i < buttonItemsLength; i++) {
-
-		var lastButton = document.getElementById("div");
-		var newParagraph = document.createElement("p");
-
-		newParagraph.innerText = withButtonItems[i].innerText;
-
-		lastButton.appendChild(newParagraph);
-		console.log(withButtonItems); 
-	}
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
 };
+
+Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+};
+
+var LGk8 = new Phone("LG", 250, "black");
+var iPhone6S = new Phone("Apple", 2250, "silver");
+var Nokia5510 = new Phone("Nokia", 50, "red");
+
+iPhone6S.printInfo();
+LGk8.printInfo();
+Nokia5510.printInfo();
