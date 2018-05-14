7b72450771cf75c595729740e2685ac6b1f42a4b
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-14 22:36:01 +0200

    Change js files

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,10 @@ Phone.prototype.printInfo = function() {
 		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 };
 
-var LGk8 = new Phone("LG", 250, "black");
+var lGk8 = new Phone("LG", 250, "black");
 var iPhone6S = new Phone("Apple", 2250, "silver");
-var Nokia5510 = new Phone("Nokia", 50, "red");
+var nokia5510 = new Phone("Nokia", 50, "red");
 
 iPhone6S.printInfo();
-LGk8.printInfo();
-Nokia5510.printInfo();
+lGk8.printInfo();
+nokia5510.printInfo();
